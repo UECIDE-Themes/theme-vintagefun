2b606575b87b7d1954902e2299c1729081e194ad
Matt Jenkins <matt@majenko.co.uk>
2015-09-05 20:43:35 +0100

    Removed debugging spam

--- src/org/uecide/themes/VintageFun.java
+++ src/org/uecide/themes/VintageFun.java
@@ -2,7 +2,6 @@ package org.uecide.themes;
 
 public class VintageFun extends ThemeControl {
     public static void init() {
-        System.err.println("Running init in VF");
         loadFont("/org/uecide/fonts/DOTMATRI.TTF");
         loadFont("/org/uecide/fonts/DOTMBold.TTF");
         loadFont("/org/uecide/fonts/DS-TERM.TTF");
